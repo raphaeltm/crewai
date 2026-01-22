780589f7e0cc4424f46a73482df82e5912fb8861
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2026-01-23 00:48:07 +0100

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -3,6 +3,7 @@
 # Run migrations
 python manage.py migrate --noinput
 python manage.py create_initial_superuser
+# End migrations
 
 # Start Daphne server
 exec daphne -b 0.0.0.0 -p 8000 config.asgi:application
